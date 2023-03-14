0418cc1b0f0fb4f8d2de6471a596456ea3b024c0
Miguel Mauricio Rivera López <91698396+MiguelMauricioRiveralopez18686@users.noreply.github.com>
2023-03-14 16:27:09 -0500

    Delete js directory

--- js/header.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.toggle-button');
-const nav = document.querySelector('nav');
-
-toggleButton.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
